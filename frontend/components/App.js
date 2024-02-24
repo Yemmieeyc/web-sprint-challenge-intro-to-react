1da9226952a8e8754d22874568402d7107b223b2
Yemmieeyc <adewole_aina@yahoo.com>
2024-02-24 14:36:04 -0800

    Done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,18 +1,53 @@
 import React from 'react'
 import axios from 'axios'
 import Character from './Character'
+import {useState, useEffect} from 'react'
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
+
 function App() {
-  // ❗ Create state to hold the data from the API
+    // ❗ Create state to hold the data from the API
+    const [planets, setPlanets] = useState([])
+    const [people, setPeople] = useState([])
+    const [combinedData, setCombinedData] = useState([])
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() =>{
+    axios.get(urlPlanets)
+    .then(res => {
+      //console.log(res.data)
+      setPlanets(res.data)
+    })
+    .catch(err => { console.error(err) })
+  })
+
+  useEffect(() => {
+    axios.get(urlPeople)
+    .then(res => {
+      setPeople(res.data)
+  
+    })
+    .catch(err => { console.error(err) })
+  })
+
+  useEffect(() => {
+    const combinedPeopleData = people.map(person => {
+      const homeworldId = person.homeworld
+      const homeWorld = planets.find(planet => planet.id === homeworldId)
+      return {...person, homeWorld}
+    })
+    setCombinedData(combinedPeopleData)
+  }, [people, planets])
+  
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {/* ❗ Map over the data in state, rendering a Character at each iteration */
+      combinedData.map(character => (
+        <Character key={Character.id} character={character} />
+      ))}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character(props) { // ❗ Add the props
+  const {character} = props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [isHomeworldRendering, setIsHomeworldRendering] = useState(false)
+   const toggle = () => { setIsHomeworldRendering(previousState => 
+    !previousState)
+    }
   return (
     <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+      {/* Use the same markup with the same attributes as in the mock */
+        <div onClick={toggle} className='character-card'>
+          <h3 className='character-name'>
+            {character.name}
+          </h3>
+          {isHomeworldRendering ?
+            <p>
+              Planet: {" "}
+              <span className='character-planet'>
+                {character.homeWorld.name}
+              </span>
+            </p>
+            : null
+          }
+        </div>}
     </div>
   )
 }
